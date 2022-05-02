e911eb95316e0bdeea8121a469a40aa3ae3ee607
meyoui <45984762+meyoui@users.noreply.github.com>
2022-05-02 10:24:35 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hoic.Mixfile do
     [
       app: :hoic,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.13.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
